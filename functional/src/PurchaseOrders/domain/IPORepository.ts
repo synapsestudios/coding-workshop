9c5fe8d74e8ca563efc02826b46bbf9e1edd89fb
Aaron Bruce <aaron@synapsestudios.com>
2023-03-28 16:26:03 -0700

    Adds to the create PO use case

--- functional/src/PurchaseOrders/domain/IPORepository.ts
+++ functional/src/PurchaseOrders/domain/IPORepository.ts
@@ -1,1 +1,8 @@
-export interface IPORepository {}
+import { Option, Result } from "oxide.ts";
+import { UUID } from "../../utilities/uuid";
+import { PurchaseOrder } from "./PurchaseOrder";
+
+export interface IPORepository {
+  save: (po: PurchaseOrder) => Promise<Result<undefined, Error>>;
+  fetch: (id: UUID) => Promise<Result<Option<PurchaseOrder>, Error>>
+}
--- functional/src/PurchaseOrders/workflows/Create-PO.ts
+++ functional/src/PurchaseOrders/workflows/Create-PO.ts
@@ -1,3 +1,8 @@
 import { IPORepository } from "../domain/IPORepository";
+import { createPurchaseOrder } from "../domain/PurchaseOrder";
 
-export const createPO = (repo: IPORepository) => () => {};
+export const createPO = ({ PORepo }: { PORepo: IPORepository }) => async () => {
+  const purchaseOrder = createPurchaseOrder();
+  const res = await PORepo.save(purchaseOrder);
+  return res.map(() => purchaseOrder.id);
+};
