0ad2262b62b5020d3342cdd070f202816eab80b8
jasperyeh <jaspyeh@gmail.com>
2024-05-14 13:58:55 -0700

    tests for creating PO with line items

--- functional/src/PurchaseOrders/domain/PurchaseOrder.spec.ts
+++ functional/src/PurchaseOrders/domain/PurchaseOrder.spec.ts
@@ -1,9 +1,25 @@
 import { isUuid } from "../../utilities/uuid";
+import { LineItem } from "./LineItem";
 import { createPurchaseOrder } from "./PurchaseOrder";
 
-describe("Purcase Order Entity", () => {
-  it("does it", () => {
-    const PO = createPurchaseOrder();
+describe("Purchase Order Entity", () => {
+  const testLineItems: LineItem[] = [
+    {
+      itemNumber: 11888,
+      description: "Nut",
+      price: 19.99,
+      quantity: 100,
+    },
+    {
+      itemNumber: 11889,
+      description: "Bolt",
+      price: 24.99,
+      quantity: 50,
+    },
+  ];
+
+  it("creates a purchase order", () => {
+    const PO = createPurchaseOrder(testLineItems);
     expect(isUuid(PO.id)).toBeTruthy();
   });
 });
--- functional/src/PurchaseOrders/workflows/Create-PO.spec.ts
+++ functional/src/PurchaseOrders/workflows/Create-PO.spec.ts
@@ -1,10 +1,26 @@
 import { match } from "oxide.ts";
 import { isUuid } from "../../utilities/uuid";
+import { LineItem } from "../domain/LineItem";
 import { constructPORepository } from "../domain/PORepository";
 import { isPurchaseOrder } from "../domain/PurchaseOrder";
 import { createPO } from "./Create-PO";
 
 describe("Create PO Workflow", () => {
+  const testLineItems: LineItem[] = [
+    {
+      itemNumber: 11888,
+      description: "Nut",
+      price: 19.99,
+      quantity: 100,
+    },
+    {
+      itemNumber: 11889,
+      description: "Bolt",
+      price: 24.99,
+      quantity: 50,
+    },
+  ];
+
   it("is callable", () => {
     const repo = constructPORepository();
     createPO({ PORepo: repo });
@@ -12,7 +28,7 @@ describe("Create PO Workflow", () => {
 
   it("returns a uuid", async () => {
     const repo = constructPORepository();
-    const poResult = await createPO({ PORepo: repo })();
+    const poResult = await createPO({ PORepo: repo })(testLineItems);
     const id = poResult.unwrap();
     expect(poResult.isOk()).toBeTruthy();
     expect(isUuid(id)).toBeTruthy();
@@ -20,7 +36,7 @@ describe("Create PO Workflow", () => {
 
   it("saves a purchase order to a repository", async () => {
     const repo = constructPORepository();
-    const result = await createPO({ PORepo: repo })();
+    const result = await createPO({ PORepo: repo })(testLineItems);
     const id = result.unwrap();
     const poRes = await repo.fetch(id);
     const output = match(poRes, {
